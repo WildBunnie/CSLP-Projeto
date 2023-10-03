5dd5d813cd516d66f2c464f5db274fca6c7c2de9
WildBunnie <wildbunnie2003@gmail.com>
2023-10-03 10:14:16 +0100

    started ex1 of deliverable 1. loading and saving of ppm images is working.

---
+++ PPMImage.cpp
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core/core.hpp>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class RGB
+{
+public:
+    char red;
+    char green;
+    char blue;
+    RGB(char r, char g, char b)
+    {
+        red = r;
+        green = g;
+        blue = b;
+    }
+};
+
+class PPMImage
+{
+public:
+    int width, height;
+    string max_color;
+    vector<vector<RGB>> pixels;
+
+    PPMImage()
+    {
+        height = 0;
+        width = 0;
+        max_color = 255;
+    }
+
+    bool loadImage(string path)
+    {
+        ifstream file(path, std::ios::binary);
+        if (!file.is_open())
+        {
+            cerr << "unable to open file" << endl;
+            return false;
+        }
+
+        string magic;
+        file >> magic;
+        if (magic != "P6")
+        {
+            cerr << "wrong format" << endl;
+            return false;
+        }
+
+        file >> width;
+        file >> height;
+        file >> max_color;
+        // remove newline
+        file.get();
+
+        pixels.resize(height, vector<RGB>(width, RGB(0, 0, 0)));
+
+        for (int row = 0; row < height; row++)
+        {
+            for (int column = 0; column < width; column++)
+            {
+                unsigned char r, g, b;
+                file.read(reinterpret_cast<char *>(&r), 1);
+                file.read(reinterpret_cast<char *>(&g), 1);
+                file.read(reinterpret_cast<char *>(&b), 1);
+                pixels[row][column] = RGB(r, g, b);
+            }
+        }
+
+        return true;
+    }
+
+    bool writeImage(string path)
+    {
+        ofstream file(path, std::ios::binary);
+        if (!file.is_open())
+        {
+            cerr << "unable to open file" << endl;
+            return false;
+        }
+
+        file << "P6" << std::endl;
+        file << width << " " << height << std::endl;
+        file << max_color << std::endl;
+
+        for (int row = 0; row < height; row++)
+        {
+            for (int column = 0; column < width; column++)
+            {
+                file.write(reinterpret_cast<char *>(&pixels[row][column].red), 1);
+                file.write(reinterpret_cast<char *>(&pixels[row][column].green), 1);
+                file.write(reinterpret_cast<char *>(&pixels[row][column].blue), 1);
+            }
+        }
+        return true;
+    }
+};
+
+// testing purposes
+// int main(int argc, char **argv)
+// {
+//     PPMImage image;
+//     image.loadImage("stop_1.ppm");
+//     image.writeImage("test.ppm");
+
+//     return 0;
+// }
