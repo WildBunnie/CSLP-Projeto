9dc35db670f7eabca8201edee9249f97605228b9
WildBunnie <wildbunnie2003@gmail.com>
2023-10-13 15:38:03 +0100

    added box filter blurring filter

--- videoPlayer/player.cpp
+++ videoPlayer/player.cpp
@@ -2,35 +2,44 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/plot.hpp>
 #include <iostream>
- 
+
 using namespace std;
 using namespace cv;
 
-static Mat gaussianBlur(Mat frame, double sigma){
-    int kernelSize = ceil(6 * sigma);
-    if (kernelSize % 2 == 0) {
-        kernelSize++;
+static Mat gaussianBlur(Mat frame, int ksize, double sigma){
+  if (ksize % 2 == 0)
+  {
+    ksize++;
+  }
+
+  int radius = ksize / 2;
+
+  Mat kernel(ksize, ksize, CV_64F);
+  double sum = 0.0;
+
+  for (int x = -radius; x <= radius; x++) {
+    for (int y = -radius; y <= radius; y++) {
+      double value = exp(-(x * x + y * y) / (2 * sigma * sigma));
+      kernel.at<double>(x + radius, y + radius) = value;
+      sum += value;
     }
+  }
 
-    int radius = kernelSize / 2;
+  kernel /= sum;
 
-    Mat kernel(kernelSize, kernelSize, CV_64F);
-    double sum = 0.0;
+  Mat result;
+  filter2D(frame, result, -1, kernel);
+  return result;
+}
 
-    for (int x = -radius; x <= radius; x++) {
-        for (int y = -radius; y <= radius; y++) {
-            double value = exp(-(x * x + y * y) / (2 * sigma * sigma));
-            kernel.at<double>(x + radius, y + radius) = value;
-            sum += value;
-        }
-    }
-
-    kernel /= sum;
-
-    Mat result;
-    // Convolve the image with the Gaussian kernel
-    filter2D(frame, result, -1, kernel);
-    return result;
+static Mat boxFilter(Mat frame, Size ksize)
+{
+  Mat kernel = Mat::ones(ksize.height, ksize.width, CV_64F);
+  kernel *= 1.0 / (ksize.width * ksize.height);
+  cout << kernel << endl;
+  Mat result;
+  filter2D(frame, result, -1, kernel);
+  return result;
 }
 
 static Mat addWatermark(Mat frame, Mat watermark, float alpha, int x, int y)
@@ -39,7 +48,7 @@ static Mat addWatermark(Mat frame, Mat watermark, float alpha, int x, int y)
     for (int col = 0; col < watermark.cols; col++){
       if (col + x < frame.rows && row + y < frame.cols)
         frame.at<Vec3b>(col + x, row + y) *= 1 - alpha;
-        frame.at<Vec3b>(col + x, row + y) += watermark.at<Vec3b>(col, row) * alpha;
+      frame.at<Vec3b>(col + x, row + y) += watermark.at<Vec3b>(col, row) * alpha;
     }
   }
   return frame;
@@ -90,41 +99,41 @@ bool player::isOpen(){
 
 Mat player::frameRgb2Yuv(Mat frame){
   for(int i = 0; i < frame.rows; i++){
-      for(int j = 0; j < frame.cols; j++){
-        uint r = frame.at<Vec3b>(i,j)[2];
-        uint g = frame.at<Vec3b>(i,j)[1];
-        uint b = frame.at<Vec3b>(i,j)[0];
+    for(int j = 0; j < frame.cols; j++){
+      uint r = frame.at<Vec3b>(i,j)[2];
+      uint g = frame.at<Vec3b>(i,j)[1];
+      uint b = frame.at<Vec3b>(i,j)[0];
 
-        uint y =  0.299*r+0.587*g+0.114*b;
-        uint cb = 128+0.564*((int)b-(int)y);
-        uint cr = 128+0.713*((int)r-(int)y);
+      uint y =  0.299*r+0.587*g+0.114*b;
+      uint cb = 128+0.564*((int)b-(int)y);
+      uint cr = 128+0.713*((int)r-(int)y);
 
-        frame.at<Vec3b>(i,j)[0] = y;
-        frame.at<Vec3b>(i,j)[1] = cb;
-        frame.at<Vec3b>(i,j)[2] = cr;
-      }
+      frame.at<Vec3b>(i,j)[0] = y;
+      frame.at<Vec3b>(i,j)[1] = cb;
+      frame.at<Vec3b>(i,j)[2] = cr;
     }
+  }
 
   return frame;
 }
 
 Mat player::frameYuv2Rgb(Mat frame){
   for(int i = 0; i < frame.rows; i++){
-      for(int j = 0; j < frame.cols; j++){
-        uint y = frame.at<Vec3b>(i,j)[0];
-        uint cb = frame.at<Vec3b>(i,j)[1];
-        uint cr = frame.at<Vec3b>(i,j)[2];
+    for(int j = 0; j < frame.cols; j++){
+      uint y = frame.at<Vec3b>(i,j)[0];
+      uint cb = frame.at<Vec3b>(i,j)[1];
+      uint cr = frame.at<Vec3b>(i,j)[2];
 
-        uint r = y+1.13983*((int)cr-128);
-        uint g = y-0.39465*((int)cb-128)-0.58060*((int)cr-128);
-        uint b = y+2.03211*((int)cb-128);
+      uint r = y+1.13983*((int)cr-128);
+      uint g = y-0.39465*((int)cb-128)-0.58060*((int)cr-128);
+      uint b = y+2.03211*((int)cb-128);
 
-        frame.at<Vec3b>(i,j)[2] = r;
-        frame.at<Vec3b>(i,j)[1] = g;
-        frame.at<Vec3b>(i,j)[0] = b;
-      }
+      frame.at<Vec3b>(i,j)[2] = r;
+      frame.at<Vec3b>(i,j)[1] = g;
+      frame.at<Vec3b>(i,j)[0] = b;
     }
-    return frame;
+  }
+  return frame;
 }
 
 void player::display(){
@@ -135,21 +144,21 @@ void player::display(){
     frame = this->frameYuv2Rgb(frame);
     imshow(this->name,frame);
     char c=(char)waitKey(25);
-      if(c==27)
-        break;
+    if(c==27)
+      break;
   }
   cap->release();
   destroyAllWindows();
 }
 
 void player::displayImage(){
-    Mat img = imread(this->name, 1);
- 
-    imshow("image", img);  
-    waitKey(0);  
-    
-    destroyAllWindows();
- }
+  Mat img = imread(this->name, 1);
+
+  imshow("image", img);
+  waitKey(0);
+
+  destroyAllWindows();
+}
 
 void player::getColorHistograms(){
   Mat frame;
@@ -160,14 +169,14 @@ void player::getColorHistograms(){
   int r, g, b;
 
   for(int i = 0; i < frame.rows; i++){
-      for(int j = 0; j < frame.cols; j++){
-          b = frame.at<Vec3b>(i,j)[0]; //blue
-          g = frame.at<Vec3b>(i,j)[1]; //green
-          r = frame.at<Vec3b>(i,j)[2]; //red
-          histogramB.insert(histogramB.begin() + b, ++histogramB[b]);
-          histogramG.insert(histogramG.begin() + g, ++histogramG[g]);
-          histogramG.insert(histogramG.begin() + r, ++histogramR[r]);
-      }
+    for(int j = 0; j < frame.cols; j++){
+      b = frame.at<Vec3b>(i,j)[0]; //blue
+      g = frame.at<Vec3b>(i,j)[1]; //green
+      r = frame.at<Vec3b>(i,j)[2]; //red
+      histogramB.insert(histogramB.begin() + b, ++histogramB[b]);
+      histogramG.insert(histogramG.begin() + g, ++histogramG[g]);
+      histogramG.insert(histogramG.begin() + r, ++histogramR[r]);
+    }
   }
 
   printHistogram(histogramR, 'r');
@@ -181,24 +190,24 @@ void player::printHistogram(vector<int> hist, char color){
   Scalar colorScalar;
 
   switch (color){
-    case 'r':
-      colorScalar = Scalar(0, 0, 255);
-      break;
-    case 'g':
-      colorScalar = Scalar(0, 255, 0);
-      break;
-    case 'b':
-      colorScalar = Scalar(255, 0, 0);
-      break;
-    default:
-      colorScalar = Scalar(0, 255, 0);
-      break;
+  case 'r':
+    colorScalar = Scalar(0, 0, 255);
+    break;
+  case 'g':
+    colorScalar = Scalar(0, 255, 0);
+    break;
+  case 'b':
+    colorScalar = Scalar(255, 0, 0);
+    break;
+  default:
+    colorScalar = Scalar(0, 255, 0);
+    break;
   }
 
   int up_height = *max_element(hist.begin(), hist.end());
 
   for(int i=0; i < hist.size(); i++){
-    p1 = Point(i*10+10, floor(up_height/100) - floor(hist[i]/100)); // right - top 
+    p1 = Point(i*10+10, floor(up_height/100) - floor(hist[i]/100)); // right - top
     p2 = Point(i*10, floor(up_height/100)); // left - bottom
     rectangle(img, p1, p2, colorScalar, 1);
   }
@@ -219,6 +228,6 @@ int main(){
   }
 
   p1.getColorHistograms();
-   
+
   return 0;
 }
