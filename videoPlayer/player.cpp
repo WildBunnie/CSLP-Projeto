8840b6e16551e94b492789ed5374e527cc6dc4c5
Bárbara Galiza <barnobregali02@gmail.com>
2023-10-17 17:18:59 +0100

    hist equalization done

--- videoPlayer/player.cpp
+++ videoPlayer/player.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/plot.hpp>
 #include <iostream>
+#include <numeric>
  
 using namespace std;
 using namespace cv;
@@ -18,11 +19,13 @@ public:
 	Mat  frameRgb2Yuv(Mat frame);
 	Mat  frameYuv2Rgb(Mat frame);
 	void displayImage();
-	bool isOpen();
+	static void getHistogram(Mat frame, int color);
+	static vector<int> getGreyHistogram(Mat frame);
 	static void getColorHistograms(Mat frame);
-	static void printHistogram(vector<int> hist, char color);
+	static void printHistogram(vector<int> hist, int color);
 	static Mat toGrayscale(Mat frame);
 	static Mat addWatermark(Mat frame, Mat watermark, int x, int y, float alpha);
+	static void histogramEqualization(Mat frame);
 };
 
 player::player(String video){
@@ -101,45 +104,59 @@ void player::displayImage(){
 		destroyAllWindows();
  }
 
-void player::getColorHistograms(Mat frame){
-	vector<int> histogramR(256,0);
-	vector<int> histogramG(256,0);
-	vector<int> histogramB(256,0);
-	int r, g, b;
+void player::getHistogram(Mat frame, int color){
+	vector<int> histogram(256,0);
+	int colorRGB;
 
 	for(int i = 0; i < frame.rows; i++){
 			for(int j = 0; j < frame.cols; j++){
-					b = frame.at<Vec3b>(i,j)[0]; //blue
-					g = frame.at<Vec3b>(i,j)[1]; //green
-					r = frame.at<Vec3b>(i,j)[2]; //red
-					histogramB.insert(histogramB.begin() + b, ++histogramB[b]);
-					histogramG.insert(histogramG.begin() + g, ++histogramG[g]);
-					histogramG.insert(histogramG.begin() + r, ++histogramR[r]);
+					colorRGB = frame.at<Vec3b>(i,j)[color]; // b = 0, g = 1, r = 2
+					histogram.insert(histogram.begin() + colorRGB, ++histogram[colorRGB]);
 			}
 	}
 
-	printHistogram(histogramR, 'r');
-	printHistogram(histogramG, 'g');
-	printHistogram(histogramB, 'b');
+	printHistogram(histogram, color);
 }
 
-void player::printHistogram(vector<int> hist, char color){
+vector<int> player::getGreyHistogram(Mat frame){
+	vector<int> histogram(256,0);
+	int itensity;
+
+	for(int i = 0; i < frame.rows; i++){
+			for(int j = 0; j < frame.cols; j++){
+					itensity = frame.at<uchar>(i,j); 
+					histogram.insert(histogram.begin() + itensity, ++histogram[itensity]);
+			}
+	}
+
+	printHistogram(histogram, 4);
+	return histogram;
+}
+
+void player::getColorHistograms(Mat frame){
+
+	for(int i = 0; i < 3; i++){
+		getHistogram(frame, i);
+	}
+}
+
+void player::printHistogram(vector<int> hist, int color){
 	Mat img = imread("background.jpg", 1);
 	Point p1, p2;
 	Scalar colorScalar;
 
 	switch (color){
-		case 'r':
+		case 2:
 			colorScalar = Scalar(0, 0, 255);
 			break;
-		case 'g':
+		case 1:
 			colorScalar = Scalar(0, 255, 0);
 			break;
-		case 'b':
+		case 0:
 			colorScalar = Scalar(255, 0, 0);
 			break;
-		default:
-			colorScalar = Scalar(0, 255, 0);
+		default: // greyscale
+			colorScalar = Scalar(255, 255, 255);
 			break;
 	}
 
@@ -157,8 +174,6 @@ void player::printHistogram(vector<int> hist, char color){
 
 	destroyAllWindows();
 }
-
-
 
 Mat player::addWatermark(Mat frame, Mat watermark, int x, int y, float alpha)
 {
@@ -184,16 +199,43 @@ Mat player::addWatermark(Mat frame, Mat watermark, int x, int y, float alpha)
 
 Mat player::toGrayscale(Mat frame)
 {
-	for (int row = 0; row < frame.rows; row++)
-	{
-		for (int col = 0; col < frame.cols; col++)
-		{
-			Vec3b pixel = frame.at<Vec3b>(row, col);
-			uchar result = pixel[0] * 0.11 + pixel[1] * 0.59 + pixel[2] * 0.30;
-			frame.at<Vec3b>(row, col) = Vec3b(result, result, result);
-		}
-	}
-	return frame;
+  Mat result(frame.rows, frame.cols, CV_8UC1);
+  for (int row = 0; row < frame.rows; row++){
+    for (int col = 0; col < frame.cols; col++){
+      Vec3b pixel = frame.at<Vec3b>(row, col);
+      result.at<uchar>(row, col) = pixel[0] * 0.11 + pixel[1] * 0.59 + pixel[2] * 0.30;
+    }
+  }
+  return result;
+}
+
+void player::histogramEqualization(Mat frame)
+{
+    cv::Mat grayscaleImage(frame.rows, frame.cols, CV_8UC1);
+	grayscaleImage = toGrayscale(frame);
+
+	Mat histogram;
+    float Range[] = {0, 256}; 
+    const float* HistRange = {Range};
+	int npixels = 256; 
+	calcHist(&grayscaleImage, 1, 0, Mat(), histogram, 1, &npixels, &HistRange, true, false);
+
+	Mat cumsum = histogram.clone();
+    for (int i = 1; i < 256; ++i) {
+        cumsum.at<float>(i) += cumsum.at<float>(i - 1);
+    }
+    cumsum /= cumsum.at<float>(npixels - 1);
+    cumsum *= 255;
+	
+	Mat equalizedImage(grayscaleImage.size(), grayscaleImage.type());
+    for (int i = 0; i < grayscaleImage.rows; ++i) {
+        for (int j = 0; j < grayscaleImage.cols; ++j) {
+            equalizedImage.at<uchar>(i, j) = cv::saturate_cast<uchar>(cumsum.at<float>(grayscaleImage.at<uchar>(i, j)));
+        }
+    }
+
+    imshow("Equalized Grayscale Image", equalizedImage);
+    waitKey(0);
 }
 
 int main(){
