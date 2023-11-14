d9208212c151c5ec43cfcb226f437c7a4ea650e7
WildBunnie <wildbunnie2003@gmail.com>
2023-11-14 16:30:36 +0000

    fixed mistake

--- src/encoder/encoder.cpp
+++ src/encoder/encoder.cpp
@@ -20,7 +20,7 @@ int jpeg_ls_predictor(int a, int b, int c){
     }
 } 
 
-Mat getEstimatesJPEG_LS(Mat image){
+Mat getResidualsJPEG_LS(Mat image){
 
 	Mat residuals(image.rows, image.cols, CV_8UC1);
 	for (int row = 0; row < image.rows; row++){
@@ -34,7 +34,6 @@ Mat getEstimatesJPEG_LS(Mat image){
 				int c = image.at<uchar>(row-1,col-1);
 				estimate = jpeg_ls_predictor(a,b,c);
 			}
-
 			residuals.at<uchar>(row,col) = image.at<uchar>(row,col) - estimate;
 		}
 	}
@@ -43,31 +42,34 @@ Mat getEstimatesJPEG_LS(Mat image){
 
 Mat getOriginalJPEG_LS(Mat residuals){
 	
-	for (int row = 0; row < residuals.rows; row++){
-		for (int col = 0; col < residuals.cols; col++){
+	Mat original = residuals.clone();
+	for (int row = 0; row < original.rows; row++){
+		for (int col = 0; col < original.cols; col++){
 			
 			int estimate = 0;
 
 			if(row > 0 && col > 0){
-				int a = residuals.at<uchar>(row,col-1);
-				int b = residuals.at<uchar>(row-1,col);
-				int c = residuals.at<uchar>(row-1,col-1);
+				int a = original.at<uchar>(row,col-1);
+				int b = original.at<uchar>(row-1,col);
+				int c = original.at<uchar>(row-1,col-1);
 				estimate =  jpeg_ls_predictor(a,b,c);
 			}
 
-			residuals.at<uchar>(row,col) = residuals.at<uchar>(row,col) + estimate;
+			original.at<uchar>(row,col) = original.at<uchar>(row,col) + estimate;
 		}
 	}
-	return residuals;
+	return original;
 }
 
 int main(int argc, char *argv[]){
-    Mat image = imread(argv[1],IMREAD_GRAYSCALE);
-	// imshow("image", image);  
-	// waitKey(0);  
-	Mat residuals = getEstimatesJPEG_LS(image);
+    Mat image = imread(argv[1],IMREAD_GRAYSCALE);  
+	Mat residuals = getResidualsJPEG_LS(image);
 	Mat result = getOriginalJPEG_LS(residuals);
+	imshow("image", image);  
+	waitKey(0);
 	imshow("image", residuals);  
+	waitKey(0);
+	imshow("image", result);  
 	waitKey(0);
     return 0;
 }
