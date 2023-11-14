0890fecf4b930ecbfe91935bb765dc219c963fe5
Bárbara Galiza <barnobregali02@gmail.com>
2023-11-14 17:17:50 +0000

    started block class

---
+++ src/hibrid-encoder/Block.hpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+class Block{
+    private:
+        int blockSize;
+        vector<vector<int>> pixels;
+    public:
+        Block(int size, vector<vector<int>> pixels);
+        int getBlockSize();
+        vector<vector<int>> getPixels;
+};
